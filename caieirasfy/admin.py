8fba87585e1fe5b338493ce88ebba5f3e2537f40
douglas <douglasklemes0303@gmail.com>
2019-09-06 17:39:39 -0300

    Adicionar Caieirasfy

---
+++ caieirasfy/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from caieirasfy.models import Musica
+
+admin.site.register(Musica)
---
+++ caieirasfy/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Musica(models.Model):
+    nome = models.CharField(
+        max_length=50,
+        verbose_name='Nome da musica'
+    )
+    Artista = models.CharField(
+        max_length=50,
+        verbose_name='Artista',
+    )
+    genero_musical = models.CharField(
+        max_length=255,
+        verbose_name='Genero musical',
+        unique=True
+    )
+    link = models.CharField(
+        max_length=255,
+        verbose_name='link da musica',
+    )
+
+    def __str__(self):
+        return self.nome + ' ' + self.Artista
